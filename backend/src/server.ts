857a66f0f3053bccbabb37393454049a1ba53d92
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-11-20 15:22:35 -0300

    feat: Implementado o arquivo .env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
 
 const app = express();
+const port = 3333;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
-app.listen(3333);
+app.listen(port, () => {
+  console.log(`🔥 Server is running on port http://localhost:${port}`);
+});
