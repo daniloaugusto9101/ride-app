1d602ff4bae04904aedfe33e6f82f475bb623ce9
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-11-20 18:40:38 -0300

    feat: Implementado utils

--- backend/src/controllers/ride-controller.ts
+++ backend/src/controllers/ride-controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import * as service from "../services/ride-service";
+import { ok } from "../utils/http-helper";
 
 export const calculateRide = async (req: Request, res: Response) => {
   const data = await service.calculateRideService();
-  res.status(200).json(data);
+  const reponse = await ok(data);
+  res.status(reponse.statusCode).json(reponse.body);
 };
