df6b206927fac83c7951ca585697271c74ac0116
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-11-23 21:14:00 -0300

    feat: backend - Update metodo /ride/confirm

--- backend/src/routes/routesV1.ts
+++ backend/src/routes/routesV1.ts
@@ -4,7 +4,7 @@ import * as RideControler from "../controllers/ride-controller";
 const router = Router();
 
 router.post("/ride/estimate", RideControler.rideEstimate);
-router.post("/ride/confirm", RideControler.rideConfirm);
+router.patch("/ride/confirm", RideControler.rideConfirm);
 router.get("/ride/:customer_id", RideControler.rideCustomerId);
 
 export default router;
