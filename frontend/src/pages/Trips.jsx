9ccc7692e9076f5c7a578d57ba7f76acc03c6314
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-11-25 13:26:42 -0300

    feat: frontend - Update layout my trips

--- frontend/src/pages/Trips.jsx
+++ frontend/src/pages/Trips.jsx
@@ -1,4 +1,15 @@
 import React from "react";
+import {
+  FaIdBadge,
+  FaCalendarAlt,
+  FaUser,
+  FaMapMarkerAlt,
+  FaRoute,
+  FaClock,
+  FaDollarSign,
+  FaTruck,
+  FaSearch,
+} from "react-icons/fa";
 import useSearchTrips from "../hooks/useSearchTrips";
 
 const Trips = () => {
@@ -18,66 +29,92 @@ const Trips = () => {
 
   return (
     <form onSubmit={handleOnSubmit}>
-      <div className="p-4">
-        <div className="mb-4">
-          <label className="block text-gray-700">
-            User ID:
-            <input
-              onChange={(event) => {
-                setCustomerId(event.target.value);
-              }}
-              value={customerId}
-              type="text"
-              className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
-              required
-            />
-          </label>
+      <div className="flex flex-col md:flex-row gap-5 p-4 justify-center">
+        <div className="relative">
+          <input
+            onChange={(event) => {
+              setCustomerId(event.target.value);
+            }}
+            value={customerId}
+            type="text"
+            placeholder="ID user..."
+            className="pl-10 pr-4 py-2 border rounded-lg w-full"
+            required
+          />
+          <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+            <FaSearch />
+          </div>
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-700">
-            Driver:
-            <input
-              onChange={(event) => {
-                setDriverId(event.target.value);
-              }}
-              value={driverId}
-              className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
-            ></input>
-          </label>
+        <div className="relative">
+          <input
+            onChange={(event) => {
+              setDriverId(event.target.value);
+            }}
+            value={driverId}
+            type="text"
+            placeholder="Driver..."
+            className="pl-10 pr-4 py-2 border rounded-lg w-full"
+          />
+          <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+            <FaUser />
+          </div>
         </div>
-        <button type="submit" className="bg-blue-500 text-white p-2 rounded-md">
-          Apply Filter
+        <button
+          type="submit"
+          className="bg-gray-800 text-white px-4 rounded-md"
+        >
+          Search
         </button>
-
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {data?.rides?.map((trip) => (
           <div
             key={trip.id}
-            className=" bg-white shadow-md rounded-lg p-4 mb-4"
+            className="bg-white shadow-lg rounded-t-md border border-gray-200 flex flex-col items-center"
           >
-            <p className="text-gray-700">
-              <strong>ID:</strong> {trip.id}
-            </p>
-            <p className="text-gray-700">
-              <strong>Date and Time:</strong> {trip.date}
-            </p>
-            <p className="text-gray-700">
-              <strong>Driver:</strong> {trip.driver.name}
-            </p>
-            <p className="text-gray-700">
-              <strong>Origin:</strong> {trip.origin}
-            </p>
-            <p className="text-gray-700">
-              <strong>Destination:</strong> {trip.destination}
-            </p>
-            <p className="text-gray-700">
-              <strong>Distance:</strong> {trip.distance}
-            </p>
-            <p className="text-gray-700">
-              <strong>Duration:</strong> {trip.duration}
-            </p>
-            <p className="text-gray-700">
-              <strong>Value:</strong> {trip.value}
-            </p>
+            {/* Imagem principal */}
+            <div className="w-full h-40 bg-gray-200 rounded-t-lg">
+              <img
+                // src="https://via.placeholder.com/300"
+                src="https://picsum.photos/seed/landscape1/300/200"
+                alt="Trip visual"
+                className="w-full h-full object-cover rounded-t-lg"
+              />
+            </div>
+
+            {/* Detalhes do Card */}
+            <div className="w-full text-center p-4">
+              <h3 className="text-lg font-bold text-gray-800 mb-2">
+                {trip.destination}
+              </h3>
+              <p className="text-sm text-gray-500 mb-2">
+                <FaCalendarAlt className="inline-block mr-2" /> {trip.date}
+              </p>
+            </div>
+
+            {/* Informações detalhadas */}
+            <div className="bg-gray-50 p-4 rounded-b-lg w-full text-left">
+              <p className="text-gray-700 mb-2">
+                <FaUser className="inline-block mr-2" />
+                <strong>Driver:</strong> {trip.driver.name}
+              </p>
+              <p className="text-gray-700 mb-2">
+                <FaMapMarkerAlt className="inline-block mr-2" />
+                <strong>Origin:</strong> {trip.origin}
+              </p>
+              <p className="text-gray-700 mb-2">
+                <FaRoute className="inline-block mr-2" />
+                <strong>Distance:</strong> {trip.distance}
+              </p>
+              <p className="text-gray-700 mb-2">
+                <FaClock className="inline-block mr-2" />
+                <strong>Duration:</strong> {trip.duration}
+              </p>
+              <p className="text-gray-700">
+                <FaDollarSign className="inline-block mr-2" />
+                <strong>Value:</strong> {trip.value}
+              </p>
+            </div>
           </div>
         ))}
       </div>
