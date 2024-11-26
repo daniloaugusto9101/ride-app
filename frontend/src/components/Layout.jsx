7c5a51e97b48b68d0387ad844f1850fdbca92700
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-11-26 12:43:08 -0300

    feat: frontend - Ajuste layout cards

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -5,8 +5,10 @@ import Menu from "./Menu";
 const Layout = ({ children }) => {
   return (
     <>
-      <Menu />
-      <main className="mx-auto px-2 py-2 ">{children}</main>
+      <main className="mx-auto px-2 py-2 flex flex-col h-screen">
+        <Menu />
+        {children}
+      </main>
     </>
   );
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,6 +15,9 @@ const Home = () => {
   const [searOrigin, setSearOrigin] = React.useState("");
   const [searchDestination, setSearchDestination] = React.useState("");
   const data = useSearchRides(searchCustomerId, searOrigin, searchDestination);
+  if (data) {
+    console.log(data);
+  }
 
   function handleOnSubmit(event) {
     // console.log("customerId: " + customerId + " origin: " + origin + " destination: " + destination);
@@ -25,8 +28,8 @@ const Home = () => {
     setIsModal(true);
   }
   return (
-    <section className="flex flex-col md:flex-row gap-y-2 gap-x-4  overflow-hidden">
-      <div className="w-full md:w-1/4  p-4">
+    <section className="flex flex-col md:flex-row gap-y-2 overflow-hidden flex-grow">
+      <div className="w-full md:w-1/4  p-4 ">
         <form className="flex flex-col gap-4 p-4 border-2 border-gray-100 rounded-lg" onSubmit={handleOnSubmit}>
           <input
             onChange={(event) => {
@@ -36,6 +39,7 @@ const Home = () => {
             type="text"
             placeholder="ID user"
             className="p-2 border border-gray-300 rounded"
+            required
           />
           <input
             onChange={(event) => {
@@ -45,6 +49,7 @@ const Home = () => {
             type="text"
             placeholder="Origem"
             className="p-2 border border-gray-300 rounded"
+            required
           />
           <input
             onChange={(event) => {
@@ -54,23 +59,40 @@ const Home = () => {
             type="text"
             placeholder="Destino"
             className="p-2 border border-gray-300 rounded"
+            required
           />
           <button type="submit" className="p-2 bg-blue-500 text-white rounded">
             Submit
           </button>
         </form>
       </div>
-      <div className="w-full md:w-3/4 flex flex-col md:flex-row py-4">
-        <div className={`${isModal ? "block" : "hidden"} w-full md:w-1/2 md:flex-1 p-4  overflow-y-scroll`}>
-          <h2>Corridas</h2>
-          {/* {rides.options.map((movie) => (
-            <Card key={movie.id} movie={movie} />
-          ))} */}
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+      <div className="w-full md:w-3/4 flex flex-col md:flex-row p-4 ">
+        <div className={`${isModal ? "block" : "hidden"} w-full md:w-1/2 md:flex-1 px-2 overflow-y-scroll`}>
+          {data?.options?.map((ride) => {
+            return (
+              <div key={ride.id} className=" shadow-md rounded-lg flex mb-2">
+                <div className="w-1/4">
+                  <img src="https://picsum.photos/id/111/300" alt="Vehicle image" className="w-full h-full object-cover rounded-l-lg" />
+                </div>
+                <div className="w-3/4 p-2 flex flex-col justify-between">
+                  <div>
+                    <h3 className="text-sm font-bold">{ride.name}</h3>
+                  </div>
+                  <div>
+                    <p className="text-gray-600">{ride.vehicle}</p>
+                    <p className="text-gray-600">"{ride.description}"</p>
+                  </div>
+                  <div>
+                    <div className="flex items-center">
+                      <span className="text-yellow-500">★★★★☆</span>
+                      <span className="ml-2 text-gray-600">({ride.review.rating})</span>
+                    </div>
+                    <div className="text-lg font-bold">R$ 50,00</div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
         <div className="w-full md:w-1/2 md:flex-1">
           {/* <img
