c191bdb1bce36aebf82c19a5a930ef10ab482087
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-11-25 21:09:15 -0300

    feat: frontend - Add maps

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import FormComponent from "../components/FormComponent";
 import Card from "../components/Card";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
 
 const Home = () => {
   return (
@@ -12,6 +13,9 @@ const Home = () => {
       <div className="w-full md:w-3/4 flex flex-col md:flex-row py-4 h-96">
         <div className="w-full md:w-1/2 md:flex-1 p-4  overflow-y-scroll ">
           <h2>Corridas</h2>
+          {/* {rides.options.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))} */}
           <Card />
           <Card />
           <Card />
@@ -19,11 +23,23 @@ const Home = () => {
           <Card />
         </div>
         <div className="w-full md:w-1/2 md:flex-1">
-          <img
+          {/* <img
             src="src/assets/maps.jpg"
             alt="Maps image"
             className="w-full h-full object-cover"
-          />
+          /> */}
+          <APIProvider apiKey={"AIzaSyC4cYuZ4ACSgOR271JbUnH7uZ4PZezyyN4"}>
+            <Map
+              style={{}}
+              defaultCenter={{
+                lat: -23.180706670183632,
+                lng: -45.869060730219715,
+              }}
+              defaultZoom={10}
+              gestureHandling={"greedy"}
+              disableDefaultUI={true}
+            />
+          </APIProvider>
         </div>
       </div>
     </section>
