dadd171a0f73b5d18900344c50cd687a42edcea1
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-11-27 13:23:44 -0300

    feat: backend - Update ride confim

--- backend/src/services/ride-service.ts
+++ backend/src/services/ride-service.ts
@@ -17,18 +17,20 @@ export const rideEstimateService = async (ride: RideModel) => {
 };
 
 export const rideConfirmService = async (ride: RideConfirmModel) => {
+  console.log("rideConfirmService", ride);
   try {
-    if (Object.keys(ride).length === 0) throw new Error();
-    if (!ride?.customer_id || ride.customer_id.trim() === "") throw new Error();
-    if (!ride?.origin || ride.origin.trim() === "") throw new Error();
-    if (!ride?.destination || ride.destination.trim() === "") throw new Error();
-    if (ride?.origin.trim().toLowerCase() === ride.destination.trim().toLowerCase()) throw new Error();
-    // opção de validação de motorista
-    // km validar
+    if (Object.keys(ride).length === 0) throw await HttpResponse.BadRequest("INVALID_DATA", "Os dados fornecidos no corpo da requisição são inválidos");
+    if (!ride?.customer_id || ride.customer_id.trim() === "") throw await HttpResponse.BadRequest("INVALID_DATA", "Os dados fornecidos no corpo da requisição são inválidos");
+    if (!ride?.origin || ride.origin.trim() === "") throw await HttpResponse.BadRequest("INVALID_DATA", "Os dados fornecidos no corpo da requisição são inválidos");
+    if (!ride?.destination || ride.destination.trim() === "") throw await HttpResponse.BadRequest("INVALID_DATA", "Os dados fornecidos no corpo da requisição são inválidos");
+    if (ride?.origin.trim().toLowerCase() === ride.destination.trim().toLowerCase()) throw await HttpResponse.BadRequest("INVALID_DATA", "Os dados fornecidos no corpo da requisição são inválidos");
+    if (!ride?.driver) throw await HttpResponse.notDriver("DRIVER_NOT_FOUND", "Motorista não encontrado");
+    // if (true) throw await HttpResponse.notKm("INVALID_DISTANCE", "Quilometragem inválida para o motorista");
+    // // km validar
     const data = await RideRepository.rideConfirm(ride);
     return await HttpResponse.ok(data);
   } catch (error) {
-    return await HttpResponse.BadRequest("INVALID_DATA", "Os dados fornecidos no corpo da requisição são inválidos");
+    return error;
   }
 };
 
--- backend/src/utils/http-helper.ts
+++ backend/src/utils/http-helper.ts
@@ -26,6 +26,28 @@ export const ok = async (data: any): Promise<HttpResponse.SuccessResponse> => {
 //   };
 // };
 
+export const notDriver = async (errorCode: string, errorDescription: string): Promise<HttpResponse.CustomErro> => {
+  return {
+    name: "notDriver",
+    message: "Driver not found",
+    statusCode: 404,
+    body: {
+      error_code: errorCode,
+      error_description: errorDescription,
+    },
+  };
+};
+export const notKm = async (errorCode: string, errorDescription: string): Promise<HttpResponse.CustomErro> => {
+  return {
+    name: "notKm",
+    message: "Km not found",
+    statusCode: 406,
+    body: {
+      error_code: errorCode,
+      error_description: errorDescription,
+    },
+  };
+};
 export const BadRequest = async (errorCode: string, errorDescription: string): Promise<HttpResponse.NotFound> => {
   return {
     statusCode: 400,
